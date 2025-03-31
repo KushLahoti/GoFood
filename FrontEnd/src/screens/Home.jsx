dc6f0d095aa7ab9274ed8da34483c64c468a5865
KushLahoti <kushlahoti123@gmail.com>
2025-04-01 00:04:44 +0530

    Initialze Project

---
+++ FrontEnd/src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <div>Body</div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
